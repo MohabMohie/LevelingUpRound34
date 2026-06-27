4da26779d1cb26d2cf0d12589144392e12392eab
Mohab Mohie <Mohab.MohieElDeen@outlook.com>
2026-06-27 10:49:09 +0300

    Enhance Basic Selenium test with URL and title logging

--- src/test/java/demo/BasicSeleniumTests.java
+++ src/test/java/demo/BasicSeleniumTests.java
@@ -2,8 +2,12 @@ package demo;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class BasicSeleniumTests {
     @Test
@@ -11,8 +15,18 @@ public class BasicSeleniumTests {
         // Test implementation
         // TestNG >> Test Suite >> Test >> Test Class >> Test Method
         WebDriver driver;
+//        RemoteWebDriver driver;
         driver = new ChromeDriver(getOptimizedOptions());
         driver.navigate().to("http://www.google.com");
+        String currentUrl = driver.getCurrentUrl();
+        System.out.println("Current URL: " + currentUrl);
+
+        String currentPageTitle = driver.getTitle();
+        System.out.println("Current Page Title: " + currentPageTitle);
+//        driver.manage().window().maximize();
+
+        // new browser session > browser action > element identification > element action > get element info / get browser info > checkpoint
+
         driver.quit();
     }
 
@@ -20,7 +34,7 @@ public class BasicSeleniumTests {
         ChromeOptions options = new ChromeOptions();
 
         // Essential execution mode
-//        options.addArguments("--headless");
+        options.addArguments("--headless");
 
         // CI/CD-specific stability
         options.addArguments("--no-sandbox");
